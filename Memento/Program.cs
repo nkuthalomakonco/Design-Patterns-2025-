9412f48127555a9478b77f0d264dab23f821fe3e
nkuthalomakonco <nkuthalomakonco@gmail.com>
2025-11-01 12:19:01 +0200

    //The Memento Pattern is a behavioral design pattern that allows you to capture and restore the internal state //    of an object without exposing its implementation details.
    //Key points:
    
    //Keeps the state of an object externally.
    
    //Allows undo/rollback functionality.
    
    //Consists of Originator, Memento, and Caretaker.

---
+++ Memento/Program.cs
@@ -0,0 +1,18 @@
+﻿//The Memento Pattern is a behavioral design pattern that allows you to capture and restore the internal state 
+//    of an object without exposing its implementation details.
+
+//Key points:
+
+//Keeps the state of an object externally.
+
+//Allows undo/rollback functionality.
+
+//Consists of Originator, Memento, and Caretaker. 
+
+class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
