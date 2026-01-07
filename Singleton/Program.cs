5ca9fe8a41db9d9e2f4bc04587c75bfa85d81edb
nkuthalomakonco <nkuthalomakonco@gmail.com>
2026-01-07 18:19:26 +0200

    Singleton

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,39 @@
 ﻿
+class Program
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+    static void Main(string[] args)
+    {
+        // The client code.
+        Singleton s1 = Singleton.Instance;
+        Singleton s2 = Singleton.Instance;
+
+        if (s1 == s2)
+        {
+            Console.WriteLine("Singleton works, both variables contain the same instance.");
+        }
+        else
+        {
+            Console.WriteLine("Singleton failed, variables contain different instances.");
+        }
+    }
+}
